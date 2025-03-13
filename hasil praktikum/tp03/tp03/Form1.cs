25c91df201cf98e3d930f9d9eb806e907b3f0b1f
Ridham2345 <idhamcholidrdn@gmail.con>
2025-03-13 15:04:35 +0700

    firsr commit

---
+++ hasil praktikum/tp03/tp03/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Inisialisasi awal UI (jika belum ada di designer)
+            this.Text = "Form Input Nama";
+            this.Size = new Size(300, 200);
+
+            // Tambahkan kontrol jika belum ada di designer
+            if (textBox2 == null)
+            {
+                textBox2 = new TextBox
+                {
+                    Name = "textBox2",
+                    Location = new Point(100, 50),
+                    Size = new Size(150, 20)
+                };
+                this.Controls.Add(textBox2);
+            }
+
+            if (label1 == null)
+            {
+                label1 = new Label
+                {
+                    Name = "label1",
+                    Location = new Point(100, 80),
+                    Size = new Size(200, 20)
+                };
+                this.Controls.Add(label1);
+            }
+
+            if (button1 == null)
+            {
+                button1 = new Button
+                {
+                    Name = "button1",
+                    Text = "Submit",
+                    Location = new Point(125, 110),
+                    Size = new Size(75, 23)
+                };
+                button1.Click += new EventHandler(button1_Click);
+                this.Controls.Add(button1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mengambil teks dari textBox2 dan menampilkannya di label1
+            string nama = textBox2.Text.Trim(); // Menghapus spasi berlebih
+            if (string.IsNullOrEmpty(nama))
+            {
+                label1.Text = "Silakan masukkan nama!";
+            }
+            else
+            {
+                label1.Text = "Halo " + nama;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Opsional: Hapus perubahan ukuran jika tidak diperlukan
+            // textBox2.Size = new Size(200, 50); // Komentari atau hapus jika tidak dibutuhkan
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Kosongkan jika tidak digunakan
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Kosongkan jika tidak digunakan
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Kosongkan jika tidak digunakan
+        }
+    }
+}
---
+++ tp_2211104016/tp_2211104016/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp_2211104016
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+          
+            this.Text = "Form Input Nama";
+            this.Size = new Size(400, 200);
+
+            if (textBox1 != null)
+            {
+                textBox1.Location = new Point(50, 50);
+                textBox1.Size = new Size(200, 20); 
+            }
+
+            if (button1 != null)
+            {
+                button1.Location = new Point(260, 48); 
+                button1.Size = new Size(75, 23);
+            }
+
+            if (label1 != null)
+            {
+                label1.Location = new Point(50, 80);
+                label1.Size = new Size(300, 20);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Halo " + textBox1.Text.Trim();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            textBox1.Size = new Size(250, 50);
+
+           
+            if (button1 != null)
+            {
+                button1.Location = new Point(textBox1.Right + 10, button1.Location.Y); 
+        
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
