8effffe1f664a5944cde8e5fc3a981c81a4400b1
Ridham2345 <idhamcholidrdn@gmail.con>
2025-03-15 17:26:08 +0700

    classbaru

--- TpModul5_2211104016/TpModul5_2211104016/Program.cs
+++ TpModul5_2211104016/TpModul5_2211104016/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace TpModul5_2211104016
 {
+    class HaloGeneric
+    {
+        public void SapaUser(string user)
+        {
+            Console.WriteLine("Halo Bro {0}", user); 
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser("Muhammad Idham"); 
         }
     }
 }
