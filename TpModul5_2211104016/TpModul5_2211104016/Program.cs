6e94bd17dcbf6db923e4578974c35182417efc5e
Ridham2345 <idhamcholidrdn@gmail.con>
2025-03-17 03:02:57 +0700

    menambahkan class penjumlahan

--- TpModul5_2211104016/TpModul5_2211104016/Program.cs
+++ TpModul5_2211104016/TpModul5_2211104016/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace TpModul5_2211104016
 {
+    class Penjumlahan
+    {
+        public T JumlahTigaAngka<T>(T a, T b, T c)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            dynamic z = c;
+            return (T)(x + y + z);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("NIM saya 2211104016");
+            Console.WriteLine("Penjumlahan tiga input angka dari 2 digit");
+
+            Penjumlahan penjumlahan = new Penjumlahan();
+            int hasil = penjumlahan.JumlahTigaAngka(22, 11, 16); 
+            Console.WriteLine("Hasil penjumlahan: " + hasil);
+            Console.ReadLine();
         }
     }
 }
