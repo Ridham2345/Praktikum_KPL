fc7e9b0f09d28aa261812b19a48a16a7a2dfca61
Ridham2345 <idhamcholidrdn@gmail.con>
2025-03-17 22:17:25 +0700

    menambahkan class SimpleDataBase

--- TpModul5_2211104016/TpModul5_2211104016/Program.cs
+++ TpModul5_2211104016/TpModul5_2211104016/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TpModul5_2211104016
 {
+    // Class Penjumlahan untuk menjumlahkan tiga angka
     class Penjumlahan
     {
         public T JumlahTigaAngka<T>(T a, T b, T c)
@@ -17,6 +15,33 @@ namespace TpModul5_2211104016
         }
     }
 
+    // Class SimpleDataBase untuk menyimpan dan menampilkan data
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.UtcNow);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -24,9 +49,23 @@ namespace TpModul5_2211104016
             Console.WriteLine("NIM saya 2211104016");
             Console.WriteLine("Penjumlahan tiga input angka dari 2 digit");
 
+            // Menggunakan class Penjumlahan
             Penjumlahan penjumlahan = new Penjumlahan();
-            int hasil = penjumlahan.JumlahTigaAngka(22, 11, 16); 
+            int hasil = penjumlahan.JumlahTigaAngka(22, 11, 16);
             Console.WriteLine("Hasil penjumlahan: " + hasil);
+
+            Console.WriteLine("\nMenyimpan dan menampilkan data dengan SimpleDataBase:");
+            // Menggunakan class SimpleDataBase
+            SimpleDataBase<int> database = new SimpleDataBase<int>();
+
+            // Menambahkan tiga data dua-digit dari NIM
+            database.AddNewData(12);
+            database.AddNewData(34);
+            database.AddNewData(56);
+
+            // Menampilkan semua data
+            database.PrintAllData();
+
             Console.ReadLine();
         }
     }
